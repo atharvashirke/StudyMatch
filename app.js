61d10dae17ad118958896ab2c002946ba4653a07
Atharva Shirke <atharvashirke@berkeley.edu>
2020-07-31 18:07:13 -0700

    Connected to MongoDB

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 // App configuration
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 
-// Routes
+// Connecting to Database
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true}, () => {
+    console.log("Connected to Database")
+}).catch(error => handleError(error));
 
+// Routes
 app.get('/', (req, res) => {
     res.render('home')
 })
